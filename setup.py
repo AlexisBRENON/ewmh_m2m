5b974e483a42e64b3859267266e07e4f175fee5a
Alexis BRENON <alexis.brenon@adyoulike.com>
2019-11-14 13:39:00 +0100

    Build long description

--- setup.py
+++ setup.py
@@ -18,6 +18,27 @@ except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
 
+from os import path
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    links = "\n".join((
+        ".. _Authors: https://ewmh-m2m.readthedocs.io/en/latest/authors.html",
+        ".. _Licence: https://ewmh-m2m.readthedocs.io/en/latest/license.html",
+        ".. _Changelog: https://ewmh-m2m.readthedocs.io/en/latest/changelog.html",
+        "",
+        ".. |logo| image:: https://ewmh-m2m.readthedocs.io/en/latest/_images/ewmh_m2m.svg",
+        "   :alt: Logo",
+        "   :width: 500",
+        ""
+    ))
+
+    with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst'), encoding='utf-8') as f:
+        lines = f.readlines()
+        start_index = lines.index(".. EndOfLinks\n")
+        description_content = "".join(lines[start_index+1:])
+
+    setup(
+        long_description=links + description_content,
+        long_description_content_type="text/x-rst; charset=UTF-8",
+        use_pyscaffold=True
+    )
